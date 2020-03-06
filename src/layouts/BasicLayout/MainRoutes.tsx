6b364350a54125a38b24ed891fbec3045b4baf9c
laiyuze <laiyuzenb@163.com>
2020-03-07 00:00:51 +0800

    tijiao

--- src/layouts/BasicLayout/MainRoutes.tsx
+++ src/layouts/BasicLayout/MainRoutes.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Redirect, Switch, Route } from 'react-router-dom';
 import routerConfig from '../../routerConfig';
 import Not from '../../pages/NotFound'
-
+// 
 class MainRoutes extends Component {
   /**
    * 渲染路由组件
